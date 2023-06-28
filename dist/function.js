2bff5cef593734eb1da8ccb0d568cf13f5699116
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-06-28 20:27:23 +0600

    function in ts

--- dist/function.js
+++ dist/function.js
@@ -1,3 +1,34 @@
 "use strict";
 // Function Types(parameter,return,anonymous/callback)
 // function signature,optional parameter/default parameter
+// implicitly any type ... no return means void
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+add(10, 20);
+// return type number
+function multiply(num1, num2) {
+    return num1[0] * num2;
+}
+var result = multiply([10], 20); // arrow 0 index 10
+console.log(result);
+// arrow function
+var divisible = function (num1, num2) {
+    return num1 / num2;
+};
+var res = divisible(10, 2);
+console.log(res);
+var mul = function (num1, num2, num3) {
+    return num1 * num2 * (num3 || 0);
+};
+var mulRes = mul(12, 13, 2);
+console.log(mulRes);
+var add1 = function () {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    return numbers.reduce(function (pre, curr) { return pre + curr; }, 0);
+};
+var result1 = add1(10, 20, 23, 23);
+console.log(result1);
--- src/function.ts
+++ src/function.ts
@@ -1,3 +1,39 @@
 // Function Types(parameter,return,anonymous/callback)
 // function signature,optional parameter/default parameter
 
+
+// implicitly any type ... no return means void
+function add(num1: number,num2: number):void{
+    console.log(num1+num2)
+}
+
+add(10,20);
+
+
+// return type number
+function multiply (num1:number[],num2:number):number{
+    return num1[0]*num2;
+}
+
+const result = multiply([10],20);// arrow 0 index 10
+console.log(result);
+
+// arrow function
+const divisible = (num1:number,num2:number):number=>{
+    return num1/num2;
+}
+
+const res = divisible(10,2);
+console.log(res);
+
+const mul = (num1:number,num2:number,num3?:number):number=>{
+    return num1 * num2 * (num3 || 0)
+}
+const mulRes = mul(12,13,2);
+console.log(mulRes);
+
+const add1 = (...numbers:number[]):number=>{
+    return numbers.reduce((pre,curr)=>pre+curr,0);
+}
+const result1 = add1(10,20,23,23);
+console.log(result1);
